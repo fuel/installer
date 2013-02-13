8dc8f69a3888ce664f8f91a08a4841eacf480686
WanWizard <wanwizard@wanwizard.eu>
2013-02-13 08:56:00 +0100

    better current master detection

--- installer.sh
+++ installer.sh
@@ -6,14 +6,17 @@ else
 
         if [ "$1" == "create" ]; then
 
-                if [ ! `which git` ]; then 
+                if [ ! `which git` ]; then
                     echo "For this installer to work you'll need to install Git."
                     echo '        http://git-scm.com/'
                 fi
 
-                current_version=`curl http://fuelphp.com/current-version`
-
-                git clone --recursive git://github.com/fuel/fuel.git "./$2" -b "$current_version/master"
+                git clone --recursive git://github.com/fuel/fuel.git "./$2"
+                cd ./$2
+                branch=`git branch -a | grep -v "remote" | grep "master" | tail -1 | cut -d/ -f3-4`
+                git checkout $branch
+                git submodule foreach git checkout $branch
+                cd ..
                 php "./$2/oil" refine install
         else
                 echo 'This is not a valid Fuel installation so Oil is a bit lost.'
